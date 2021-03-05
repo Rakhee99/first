7b9191f2cb754b2c371b0ff669fa2ab5edcf483e
rakhee99 <rakheemagic99@gmail.com>
2021-03-05 10:29:41 +0530

    first commit

---
+++ classses.py
@@ -0,0 +1,10 @@
+class computer:
+    def __init__(self,name,roll):
+        self.name=name
+        self.roll=roll
+    def addroll(roll):
+        return self.roll+10
+name=input("enter name")
+roll=int(input("enter roll"))
+com=computer(name,roll)
+print(com.addroll(roll))
